a0d0070db6860a112b658455229072a01582b4fb
Colin Egerer <cpegerer@email.wm.edu>
2021-02-27 17:42:45 -0600

    Implement Taylor expansions of (pITD->PDF Kernel)*(Jacobi PDF parametrization)

--- kernel.h
+++ kernel.h
@@ -40,10 +40,35 @@ namespace PITD
   double betaFn(double v, double w);
 
   /*
-    Jacobi Polynomial
+    Jacobi coefficients
+    w_{n,j}^{(a,b)} = (-1)^j/n! * (n_C_j) * { Gamma(a+n+1)*Gamma(a+b+n+j+1) }/{ Gamma(a+j+1)*Gamma(a+b+n+1) }
+  */
+  double coeffJacobi(int n, int j, double a, double b);
+
+  /*
+    Jacobi Polynomial:  \omega_n^{(a,b)}(x) = \sum_{j=0}^n coeffJacobi(n,j,a,b)*x^j
   */
   double jacobi(int n, double a, double b, double x);
 
+  /*
+    Support for Taylor expansion of NLO pITD->PDF Matching Kernel
+  */
+  double texp_cn(int n, int z);
+
+  /*
+    Support for Taylor expansion of DGLAP Kernel
+  */
+  double texp_gn(int n);
+
+  /*
+    Support for Taylor expansion of Lattice-MSbar matching kernel
+  */
+  double texp_dn(int n);
+
+  /*
+    Support for Taylor expansion of (pITD->PDF Kernel)*(Jacobi PDF Parametrization)
+  */
+  double pitd_texp_sigma_n(int n, int trunc, double a, double b, double nu);
 
 }
 #endif
